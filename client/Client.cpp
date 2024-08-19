7109302e7a4dba4e435d36cf8bc2b05803f0220c
dx <vl-mr_123@mail.ru>
2024-08-19 23:30:36 +0300

    добавлен класс tcp сервера

---
+++ client/Client.cpp
@@ -0,0 +1,69 @@
+#include "Client.h"
+
+TCPClient::TCPClient(const std::string& host, int port)
+    : host_(host), port_(port), socket_(INVALID_SOCKET) {
+    // Инициализация WinSock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Ошибка инициализации WinSock." << std::endl;
+        throw std::runtime_error("WSAStartup failed");
+    }
+}
+
+TCPClient::~TCPClient()
+{
+    if (socket_ != INVALID_SOCKET) {
+        closesocket(socket_);
+    }
+    WSACleanup();
+}
+
+void TCPClient::connect()
+{
+    // Создание сокета
+    socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (socket_ == INVALID_SOCKET) {
+        std::cerr << "Ошибка создания сокета." << std::endl;
+        throw std::runtime_error("Socket creation failed");
+    }
+
+    // Настройка адреса сервера
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port_);
+
+    // Использование inet_pton для преобразования IP-адреса
+    if (inet_pton(AF_INET, host_.c_str(), &serverAddress.sin_addr) <= 0) {
+        std::cerr << "Ошибка преобразования IP-адреса." << std::endl;
+        throw std::runtime_error("inet_pton failed");
+    }
+
+    // Подключение к серверу
+    if (::connect(socket_, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Ошибка подключения к серверу." << std::endl;
+        throw std::runtime_error("Connection failed");
+    }
+    std::cout << "Соединение с сервером установлено." << std::endl;
+}
+void TCPClient::sendMessage(const std::string& message)
+{
+    if (send(socket_, message.c_str(), message.length(), 0) == SOCKET_ERROR) {
+        std::cerr << "Ошибка отправки сообщения." << std::endl;
+        throw std::runtime_error("Send failed");
+    }
+    std::cout << "Сообщение отправлено." << std::endl;
+}
+
+std::string TCPClient::receiveMessage()
+{
+    const int bufferSize = 1024;
+    char buffer[bufferSize];
+    int received = recv(socket_, buffer, bufferSize - 1, 0);
+    if (received == SOCKET_ERROR) {
+        std::cerr << "Ошибка получения сообщения." << std::endl;
+        throw std::runtime_error("Receive failed");
+    }
+    buffer[received] = '\0';  // Добавление нуль-терминатора
+    return std::string(buffer);
+}
+
---
+++ client/Client.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+class TCPClient {
+public:
+    TCPClient(const std::string& host, int port);
+
+    ~TCPClient();
+
+    void connect();
+
+    void sendMessage(const std::string& message);
+
+    std::string receiveMessage();
+
+private:
+    std::string host_;
+    int port_;
+    SOCKET socket_;
+};
