968fd3ce6b679738c5875561e7160193f8dda9d0
dx <vl-mr_123@mail.ru>
2024-08-20 02:35:41 +0300

    добавлена логика получения имени и домена компьютера

--- client/main.cpp
+++ client/main.cpp
@@ -8,6 +8,22 @@
 
 using namespace std;
 
+string getPCName() {
+    char pcName[200];
+    unsigned long pcNameLen = 200;
+    GetComputerNameA(pcName, &pcNameLen);
+    return string(pcName);
+}
+
+string getPCDomain() {
+    CHAR domain[MAX_PATH];
+    DWORD size = MAX_PATH;
+    string domainName = "";
+    if (GetComputerNameExA(ComputerNameDnsDomain, domain, &size))
+        domainName = domain;
+    return domainName;
+}
+
 int main() {
 
     ConfigReader cr;
@@ -17,6 +33,9 @@ int main() {
     int server_port = atoi(conf["control_server"].substr(conf["control_server"].find(":") + 1).c_str());
     string screenshot_dir = conf["screenshot_dir"];
     int send_status_timer = atoi(conf["timer_send_status"].c_str());
- 
+    string host_name = getPCName();
+    string host_domain_name = getPCDomain();
+
+
 
 }
