6a3594411c9df15f4e027eb3be1d58d003c8b3da
dx <vl-mr_123@mail.ru>
2024-08-20 15:43:37 +0300

    доработана обработка принятия соединения сервером

--- server/Server.h
+++ server/Server.h
@@ -4,55 +4,65 @@
 #include <winsock2.h>
 #include <windows.h>
 #include <thread>
+#include <vector>
 #include <ws2tcpip.h>
+
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
 
 #pragma comment(lib, "ws2_32.lib") // Линковка с библиотекой ws2_32.lib
 
+enum Command {
+    GET_STATE,
+    GET_SCREENSHOT
+};
+
+struct ClientData {
+    SOCKET sock = 0;
+
+    string ip;
+    string hostName;
+    string domain;
+    string lastActivityTime;
+    string screenshotPath;
+};
+
 class Server {
-public:
-    Server(uint16_t port) : port_(port), listen_socket_(INVALID_SOCKET) {}
-
-    bool start() {
-        WSADATA wsaData;
-        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-            std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
-            return false;
-        }
-
-        listen_socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (listen_socket_ == INVALID_SOCKET) {
-            std::cerr << "socket() failed with error: " << WSAGetLastError() << std::endl;
-            WSACleanup();
-            return false;
-        }
-
-        sockaddr_in server_addr;
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = INADDR_ANY;
-        server_addr.sin_port = htons(port_);
-
-        if (bind(listen_socket_, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-            std::cerr << "bind() failed with error: " << WSAGetLastError() << std::endl;
-            closesocket(listen_socket_);
-            WSACleanup();
-            return false;
-        }
-
-        if (listen(listen_socket_, SOMAXCONN) == SOCKET_ERROR) {
-            std::cerr << "listen() failed with error: " << WSAGetLastError() << std::endl;
-            closesocket(listen_socket_);
-            WSACleanup();
-            return false;
-        }
-
-        std::cout << "Server is running on port " << port_ << "..." << std::endl;
-        accept_connections();
-        return true;
-    }
-
 private:
     uint16_t port_;
     SOCKET listen_socket_;
+    vector<ClientData> cld;
+
+    ClientData parse_client_data(const std::string& input, SOCKET sock) {
+        ClientData clientData;
+        std::istringstream stream(input);
+        std::string line;
+
+        std::unordered_map<std::string, std::string*> fieldMap = {
+            {"hostname:", &clientData.hostName},
+            {"domain:", &clientData.domain},
+            {"last_activ_time:", &clientData.lastActivityTime},
+            {"ip:", &clientData.ip}
+        };
+
+        while (std::getline(stream, line)) {
+            size_t delimiterPos = line.find(':');
+            if (delimiterPos != std::string::npos) {
+                std::string key = line.substr(0, delimiterPos + 1);
+                std::string value = line.substr(delimiterPos + 1);
+                value.erase(0, value.find_first_not_of(" \t"));
+                value.erase(value.find_last_not_of(" \t") + 1);
+                if (fieldMap.find(key) != fieldMap.end()) {
+                    *(fieldMap[key]) = value;
+                }
+            }
+        }
+
+        clientData.sock = sock;
+        return clientData;
+    }
 
     void accept_connections() {
         while (true) {
@@ -68,12 +78,79 @@ private:
             char client_ip[INET_ADDRSTRLEN];
             if (inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip)) != nullptr) {
                 std::cout << "Client connected from: " << client_ip << std::endl;
+                send(client_socket, "GET_STATE", 10, 0);
+
+                char buff[1024] = {};
+                recv(client_socket, buff, 1024, 0);
+
+                if (string(buff) == "STATE START") {
+                    ZeroMemory(buff, 1024);
+                    recv(client_socket, buff, 1024, 0);
+                    auto data = parse_client_data(string(buff), client_socket);
+
+                    ZeroMemory(buff, 1024);
+                    recv(client_socket, buff, 1024, 0);
+                    if (string(buff) == "STATE END") {
+                        cld.push_back(data);
+                    }
+                }
             }
             else {
                 std::cerr << "inet_ntop() failed with error: " << WSAGetLastError() << std::endl;
             }
 
+
         }
     }
+public:
+    Server(uint16_t port) : port_(port), listen_socket_(INVALID_SOCKET) {}
+
+    bool start() {
+        thread *th = new thread([this]() {
+            WSADATA wsaData;
+            if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+                std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
+                return false;
+            }
+
+            listen_socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if (listen_socket_ == INVALID_SOCKET) {
+                std::cerr << "socket() failed with error: " << WSAGetLastError() << std::endl;
+                WSACleanup();
+                return false;
+            }
+
+            sockaddr_in server_addr;
+            server_addr.sin_family = AF_INET;
+            server_addr.sin_addr.s_addr = INADDR_ANY;
+            server_addr.sin_port = htons(port_);
+
+            if (bind(listen_socket_, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+                std::cerr << "bind() failed with error: " << WSAGetLastError() << std::endl;
+                closesocket(listen_socket_);
+                WSACleanup();
+                return false;
+            }
+
+            if (listen(listen_socket_, SOMAXCONN) == SOCKET_ERROR) {
+                std::cerr << "listen() failed with error: " << WSAGetLastError() << std::endl;
+                closesocket(listen_socket_);
+                WSACleanup();
+                return false;
+            }
+
+            std::cout << "Server is running on port " << port_ << "..." << std::endl;
+            accept_connections();
+            return true;
+            });
+
+        th->detach();
+        return true;
+    }
+
+   
+
+
+
 };
 
